fbdb6e0384f7668f35b588af5b53f417fcb21e73
LloyolaB <luis.loyola.b@gmail.com>
2022-09-09 22:24:26 -0300

    apellidos en la clase persona

--- clases.py
+++ clases.py
@@ -1,12 +1,14 @@
 class Persona:
-    def __init__(self, nombre, edad):
+    def __init__(self, nombre, apellido, edad):
         self.nombre = nombre
         self.edad = edad
+        self.apellido = apellido
 
     def __str__(self):
-        return self.nombre + " " + str(self.edad)
+        return self.nombre + " " + str(self.edad) + " " + self.apellido
 
 
 persona1 = Persona("Juan", 20)
 print(persona1)
 persona2 = Persona("Pedro", 30)
+persona3 = Persona("Maria","Perez", 40)
